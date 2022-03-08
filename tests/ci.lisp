4b39e48d2d5b1337deb652c5d26166c86a1ec409
Vasily Postnicov <shamaz.mazum@gmail.com>
2022-03-08 18:09:33 +0300

    AAA

--- tests/ci.lisp
+++ tests/ci.lisp
@@ -1,7 +1,7 @@
 (defun do-all()
-  (ql:quickload :sandcorn-distribution/tests :verbose t)
+  (ql:quickload :porous-media-segmentation/tests :verbose t)
   (uiop:quit
-   (if (uiop:call-function "sandcorn-distribution-tests:run-tests")
+   (if (uiop:call-function "pm-segmentation-tests:run-tests")
        0 1)))
 
 (do-all)
